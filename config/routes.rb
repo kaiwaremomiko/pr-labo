2ae769ae4994ee2492547e047aa41fe35f6fd4e4
kaiwaremomiko <sprout_will_be_bloom@yahoo.co.jp>
2020-04-30 01:23:48 +0900

    sitemap

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # post   '/login',   to: 'sessions#create'
   # delete '/logout',  to: 'sessions#destroy'
   root to: 'home#index'
-  get 'sitemap', to: redirect('https://s3-us-east-2.amazonaws.com/prlabbacket/sitemaps/sitemap.xml.gz')
+  get 'sitemap1.xml', to: redirect('https://s3-us-east-2.amazonaws.com/prlabbacket/sitemaps/sitemap1.xml.gz')
+  get 'sitemap2.xml', to: redirect('https://s3-us-east-2.amazonaws.com/prlabbacket/sitemaps/sitemap2.xml.gz')
   resources :npos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
