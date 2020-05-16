4554f2f9bdf138e2385690ee72011f5b62cd0550
kaiwaremomiko <sprout_will_be_bloom@yahoo.co.jp>
2020-05-17 00:16:08 +0900

    company sitemap

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   root to: 'home#index'
   # get 'sitemap1', to: redirect('https://s3-us-east-2.amazonaws.com/prlabbacket/sitemaps/sitemap1.xml.gz')
   # get 'sitemap2', to: redirect('https://s3-us-east-2.amazonaws.com/prlabbacket/sitemaps/sitemap2.xml.gz')
-  get 'sitemap', to: redirect('https://s3-us-east-2.amazonaws.com/prlabbacket/sitemaps/sitemap.xml.gz')
+  get 'sitemap', to: redirect('https://prlabbacket.s3.us-east-2.amazonaws.com/sitemaps/sitemap.xml.gz')
+  get 'sitemap2', to: redirect('https://s3-us-east-2.amazonaws.com/prlabbacket/sitemaps2/sitemap.xml.gz')
   resources :npos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- config/sitemap.rb
+++ config/sitemap.rb
@@ -2,7 +2,7 @@
 SitemapGenerator::Sitemap.default_host = "https://www.pr-labo.net"
 SitemapGenerator::Sitemap.sitemaps_host = "https://s3-us-east-2.amazonaws.com/#{ENV['AWS_S3_BUCKET']}/"
 SitemapGenerator::Sitemap.public_path = 'tmp/'
-SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
+SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps2/'
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(fog_provider: 'AWS',
   aws_access_key_id: ENV['AWS_S3_ACCESS_KEY_ID'],
   aws_secret_access_key: ENV['AWS_S3_SECRET_ACCESS_KEY'],
@@ -11,25 +11,25 @@ SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(fog_provider
 
 
 SitemapGenerator::Sitemap.create do
-  add root_path
+  # add root_path
 
-  add npos_path, :priority => 0.7, :changefreq => 'daily' 
+  # add npos_path, :priority => 0.7, :changefreq => 'daily' 
 
-  Npo.find_each do |npo|
-    add npo_path(npo), lastmod: npo.updated_at
-  end
+  # Npo.find_each do |npo|
+  #   add npo_path(npo), lastmod: npo.updated_at
+  # end
 
   add companies_path, :priority => 0.7, :changefreq => 'daily' 
 
-  # Company.find_each do |company|
-  #   add company_path(company), lastmod: company.updated_at
-  # end
-
-  add occupations_path, :priority => 0.7, :changefreq => 'daily' 
-
-  Occupation.find_each do |occupation|
-    add occupation_path(occupation), lastmod: occupation.updated_at
+  Company.find_each do |company|
+    add company_path(company), lastmod: company.updated_at
   end
+
+  # add occupations_path, :priority => 0.7, :changefreq => 'daily' 
+
+  # Occupation.find_each do |occupation|
+  #   add occupation_path(occupation), lastmod: occupation.updated_at
+  # end
 
 
   # Put links creation logic here.
